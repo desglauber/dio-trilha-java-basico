0361be548f9344fe7965c1a0eba2895ee49026b8
Glauber Farias <desglauber@gmail.com>
2024-03-26 12:56:00 -0400

    implementação da funcionalidade entrando em contato

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -5,7 +6,36 @@ public class ProcessoSeletivo {
         System.out.println("Processo Seletivo 2024");
         imprimirSelecionados();
         selecaoCandidatos();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+    static void entrandoEmContato (String candidato){
+        int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+    }
+
+    //método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
 
     static void imprimirSelecionados() {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
