271a3cc83935c7b94d3db494f3d78c61831923b3
Glauber Farias <desglauber@gmail.com>
2024-04-24 11:30:23 -0400

    Operações Bancárias Simples com Switch

---
+++ Desafios/src/SimulacaoBancaria.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        double saldo = 0; 
+        // Loop infinito para manter o programa em execução até que o usuário decida sair
+        while (true) {
+            int opcao = scanner.nextInt(); 
+        // TODO: Implemente as condições necessárias para avaliaa a opção escolhida:
+        // Dica: Utilze o switch/case para o programa realizar as operações escolhidas pelo usuário.
+            switch (opcao) {
+                case 1:
+                    double depositar = scanner.nextDouble();
+                    saldo += depositar;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                case 2:                    
+                    double sacar = scanner.nextDouble();
+                    if(sacar > 0 && sacar <= saldo){
+                        saldo -= sacar;
+                        System.out.println("Saldo atual: " + saldo);
+                    } else {
+                        System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saldo atual: "+ saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                return;
+        // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
+                default:
+                System.out.println("Opção inválida.");
+            }         
+        }
+    }
+}
